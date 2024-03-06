8109d985e0c894dfa4c4b8d4e42254fac4663329
tedbu <124395912+tedbu@users.noreply.github.com>
2024-03-06 15:56:21 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,37 @@
+input.onButtonPressed(Button.A, function () {
+    wuKong.setMotorSpeed(wuKong.MotorList.M1, -50)
+    wuKong.setMotorSpeed(wuKong.MotorList.M2, -50)
+    basic.pause(500)
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S1, 210)
+    wuKong.stopAllMotor()
+    rainwater = false
+})
+input.onButtonPressed(Button.B, function () {
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S1, 360)
+    basic.pause(500)
+    wuKong.setMotorSpeed(wuKong.MotorList.M1, 50)
+    wuKong.setMotorSpeed(wuKong.MotorList.M2, 50)
+    basic.pause(500)
+    wuKong.stopAllMotor()
+    rainwater = true
+})
+let rainwater = false
+rainwater = false
+basic.forever(function () {
+    if (Environment.ReadWaterLevel(AnalogPin.P1) > 10) {
+        if (rainwater == false) {
+            wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S1, 360)
+            basic.pause(500)
+            wuKong.setMotorSpeed(wuKong.MotorList.M1, 50)
+            wuKong.setMotorSpeed(wuKong.MotorList.M2, 50)
+            basic.pause(500)
+            wuKong.stopAllMotor()
+            rainwater = true
+        }
+    }
+    if (Environment.ReadWaterLevel(AnalogPin.P1) < 10) {
+        if (rainwater == true) {
+            rainwater = false
+        }
+    }
+})
