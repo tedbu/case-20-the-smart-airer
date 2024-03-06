ace41138724cbd8b72bf20b02eb623658b929725
tedbu <124395912+tedbu@users.noreply.github.com>
2024-03-06 16:34:52 +0100

    update timing

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 input.onButtonPressed(Button.A, function () {
     wuKong.setMotorSpeed(wuKong.MotorList.M1, -50)
     wuKong.setMotorSpeed(wuKong.MotorList.M2, -50)
-    basic.pause(400)
+    basic.pause(350)
     wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S1, 210)
     wuKong.stopAllMotor()
     rainwater = false
@@ -11,12 +11,14 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(500)
     wuKong.setMotorSpeed(wuKong.MotorList.M1, 50)
     wuKong.setMotorSpeed(wuKong.MotorList.M2, 50)
-    basic.pause(400)
+    basic.pause(350)
     wuKong.stopAllMotor()
     rainwater = true
 })
 let rainwater = false
 basic.showIcon(IconNames.Ghost)
+basic.pause(2000)
+basic.showString("Hello!")
 rainwater = false
 basic.forever(function () {
     if (Environment.ReadWaterLevel(AnalogPin.P1) > 10) {
@@ -25,7 +27,7 @@ basic.forever(function () {
             basic.pause(500)
             wuKong.setMotorSpeed(wuKong.MotorList.M1, 50)
             wuKong.setMotorSpeed(wuKong.MotorList.M2, 50)
-            basic.pause(400)
+            basic.pause(350)
             wuKong.stopAllMotor()
             rainwater = true
         }
